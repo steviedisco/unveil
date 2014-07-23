730ff0a261e3727da327e8f9c004b94d23b8f064
Steve Driscoll <steviedisco@gmail.com>
2014-07-23 22:43:33 +0000

    added basic facebook authentication

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // entry point to application
 
-// main dependencies
+// dependencies
 var express = require('express');
 var http = require('http');
 var path = require('path');
@@ -8,6 +8,8 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var facebook = require('facebook-node-sdk');
+var facebookConfig = require('./config/facebook');
 
 // set up app
 var app = express();
@@ -20,18 +22,28 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
 // load middleware
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(app.router);
+app.configure(function () {
+    app.use(favicon());
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded());
+    app.use(cookieParser());
+    app.use(express.session({ secret: 'foo bar'}));
+    app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(facebook.middleware(facebookConfig));
+});
 
 // setup routes
-var routes = require('./routes');
-app.get('/', routes.index);
+// var routes = require('./routes');
+// app.get('/', routes.index);
+app.get('/', facebook.loginRequired(), function (req, res) {
+    console.log('test');
+    req.facebook.api('/me', function (err, user) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello, ' + user.name + '!');
+    });
+});
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
---
+++ config/facebook.js
@@ -0,0 +1,4 @@
+﻿module.exports = {
+    appId: '935143883178297',
+    secret: 'ec6aee20e98ab5c359acb7f0e4d87607'
+}
