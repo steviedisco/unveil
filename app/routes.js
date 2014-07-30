f5a97ce0b9d1bc9986450979943c166559b51a3a
Steve Driscoll <steviedisco@gmail.com>
2014-07-30 21:07:46 +0000

    SD - added facebook linking

--- app/routes.js
+++ app/routes.js
@@ -57,22 +57,86 @@ module.exports = function (app, passport) {
     });
     
     // =====================================
+    // FACEBOOK ROUTES =====================
+    // =====================================
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+    
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+		passport.authenticate('facebook', {
+        successRedirect : '/profile',
+        failureRedirect : '/'
+    }));
+    
+    // =====================================
     // LOGOUT ==============================
     // =====================================
     app.get('/logout', function (req, res) {
         req.logout();
         res.redirect('/');
     });
+
+    // =============================================================================
+    // AUTHORIZE (ALREADY LOGGED IN / CONNECTING OTHER SOCIAL ACCOUNT) =============
+    // =============================================================================
+    
+    // locally --------------------------------
+    app.get('/connect/local', function (req, res) {
+        res.render('connect-local.ejs', { message: req.flash('loginMessage') });
+    });
+    app.post('/connect/local', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+    
+    // facebook -------------------------------
+    
+    // send to facebook to do the authentication
+    app.get('/connect/facebook', passport.authorize('facebook', { scope : 'email' }));
+    
+    // handle the callback after facebook has authorized the user
+    app.get('/connect/facebook/callback',
+			passport.authorize('facebook', {
+        successRedirect : '/profile',
+        failureRedirect : '/'
+    }));
+    
+    // route middleware to make sure a user is logged in
+    function isLoggedIn(req, res, next) {
+        
+        // if user is authenticated in the session, carry on 
+        if (req.isAuthenticated())
+            return next();
+        
+        // if they aren't redirect them to the home page
+        res.redirect('/');
+    }
+
+    // =============================================================================
+    // UNLINK ACCOUNTS =============================================================
+    // =============================================================================
+    // used to unlink accounts. for social accounts, just remove the token
+    // for local account, remove email and password
+    // user account will stay active in case they want to reconnect in the future
+    
+    // local -----------------------------------
+    app.get('/unlink/local', function (req, res) {
+        var user = req.user;
+        user.local.email = undefined;
+        user.local.password = undefined;
+        user.save(function (err) {
+            res.redirect('/profile');
+        });
+    });
+    
+    // facebook -------------------------------
+    app.get('/unlink/facebook', function (req, res) {
+        var user = req.user;
+        user.facebook.token = undefined;
+        user.save(function (err) {
+            res.redirect('/profile');
+        });
+    });
 };
-
-// route middleware to make sure a user is logged in
-function isLoggedIn(req, res, next) {
-    
-    // if user is authenticated in the session, carry on 
-    if (req.isAuthenticated())
-        return next();
-    
-    // if they aren't redirect them to the home page
-    res.redirect('/');
-}
-
---
+++ config/auth.js
@@ -0,0 +1,23 @@
+﻿// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+    
+    'facebookAuth' : {
+        'clientID' 		: '935143883178297', 
+        'clientSecret' 	: 'ec6aee20e98ab5c359acb7f0e4d87607', // your App Secret
+        'callbackURL' 	: 'http://localhost:1337/auth/facebook/callback'
+    },
+    
+    'twitterAuth' : {
+        'consumerKey' 		: 'your-consumer-key-here',
+        'consumerSecret' 	: 'your-client-secret-here',
+        'callbackURL' 		: 'http://localhost:1337/auth/twitter/callback'
+    },
+    
+    'googleAuth' : {
+        'clientID' 		: 'your-secret-clientID-here',
+        'clientSecret' 	: 'your-client-secret-here',
+        'callbackURL' 	: 'http://localhost:1337/auth/google/callback'
+    }
+};
